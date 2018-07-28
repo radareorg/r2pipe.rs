5ddfda5a5d889631c87afb338443948a237b8cf0
HMPerson1 <hmperson1@gmail.com>
2018-07-27 23:26:02 -0400

    Add support TCP connections

---
+++ examples/tcp.rs
@@ -0,0 +1,18 @@
+extern crate r2pipe;
+extern crate serde_json;
+
+use r2pipe::R2Pipe;
+
+fn main() {
+    let mut r2p = R2Pipe::tcp("localhost:9080").unwrap();
+
+    println!("{}", r2p.cmd("?e Hello World").unwrap());
+
+    let json = r2p.cmdj("ij").unwrap();
+    println!("{}", serde_json::to_string_pretty(&json).unwrap());
+    println!("ARCH {}", json["bin"]["arch"]);
+    println!("BITS {}", json["bin"]["bits"]);
+    println!("Disasm:\n{}", r2p.cmd("pd 20").unwrap());
+    println!("Hexdump:\n{}", r2p.cmd("px 64").unwrap());
+    r2p.close();
+}
