2494daf0abee57352c90977b35a34d928b57d661
kriw <kotarou777775@gmail.com>
2018-08-11 01:06:05 +0900

    Add example

---
+++ examples/http.rs
@@ -0,0 +1,16 @@
+extern crate r2pipe;
+extern crate serde_json;
+
+use r2pipe::R2Pipe;
+
+fn main() {
+    let mut r2p = R2Pipe::http("http://localhost:9080").unwrap();
+
+    let json = r2p.cmdj("ij").unwrap();
+    println!("{}", serde_json::to_string_pretty(&json).unwrap());
+    println!("ARCH {}", json["bin"]["arch"]);
+    println!("BITS {}", json["bin"]["bits"]);
+    println!("Disasm:\n{}", r2p.cmd("pd 20").unwrap());
+    println!("Hexdump:\n{}", r2p.cmd("px 64").unwrap());
+    r2p.close();
+}
