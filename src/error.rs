27e94a00a4e6dc61cc7995da985f26e0ddd628e6
meme <meme@users.noreply.github.com>
2021-05-21 08:28:46 -0400

    Update Error for reqwest

--- src/error.rs
+++ src/error.rs
@@ -2,42 +2,50 @@ use std::sync::mpsc::{RecvError, SendError, TryRecvError};
 use std::{io, str};
 use thiserror::Error;
 
-/// Custom `Error` for r2pipe.rs
+#[cfg(feature = "http")]
+use reqwest;
+
+/// Custom `Error` for r2pipe.rs.
 #[derive(Error, Debug)]
 pub enum Error {
-    /// An I/O error occurred
+    /// An I/O error occurred.
     #[error("I/O error")]
     Io(#[from] io::Error),
 
-    /// No open radare2 session, perhaps path was not specified
+    /// No open radare2 session, perhaps path was not specified.
     #[error("No open session")]
     NoSession,
 
-    /// Response had invalid/missing JSON
+    /// Response had invalid/missing JSON.
     #[error("Empty response from JSON")]
     EmptyResponse,
 
-    /// Incorrect number of arguments, or incorrect format
+    /// Incorrect number of arguments, or incorrect format.
     #[error("Argument mismatch")]
     ArgumentMismatch,
 
-    /// An error occurred inside of serde
+    /// An error occurred inside of serde.
     #[error("Serde deserialization error")]
     SerdeError(#[from] serde_json::Error),
 
-    /// Error during UTF-8 decoding
+    /// Error during UTF-8 decoding.
     #[error("UTF-8 decoding error")]
     Utf8(#[from] str::Utf8Error),
 
-    /// Error receiving data from channel
+    /// Error receiving data from channel.
     #[error("Receive channel data error")]
     ChannelReceiveError(#[from] RecvError),
 
-    /// Error trying to receive data from channel
+    /// Error trying to receive data from channel.
     #[error("Trying receive channel data error")]
     ChannelTryReceiveError(#[from] TryRecvError),
 
-    /// Error sending data through channel
+    /// Error sending data through channel.
     #[error("Send channel data error")]
     ChannelSendError(#[from] SendError<String>),
+
+    /// Error during reqwest operation.
+    #[cfg(feature = "http")]
+    #[error("Reqwest error")]
+    Reqwest(#[from] reqwest::Error),
 }
